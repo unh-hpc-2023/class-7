17c78d2b07046607dd4629267b407b7535cccc9a
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-14 13:02:13 -0500

    bowling: add first test (FAILS)
    It fails to even compile, since there is no bowlingScore() function yet.

--- bowling.cxx
+++ bowling.cxx
@@ -1,1 +1,9 @@
 #include <gtest/gtest.h>
+
+TEST(Bowling, AllZeros)
+{
+  std::vector<int> rolls = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  };
+  EXPECT_EQ(bowlingScore(rolls), 0);
+}
